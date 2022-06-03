52f870be8a8fa91ec7939f0260adb63e41e8993f
devPinheiro <pinheirolaoluwa@gmail.com>
2022-06-03 03:58:18 +0100

    initial commit

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS", ["450b5ce0521d9d15", "809bb8920e17260b"]),
   },
 });
